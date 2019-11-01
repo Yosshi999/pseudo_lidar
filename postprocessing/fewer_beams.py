f6019b675a0239c676209c9831f47599087f93aa
Yosshi999 <Yosshi999@users.noreply.github.com>
2019-11-01 23:18:34 +0900

    bugfix: drop lidar points outside left image

--- postprocessing/fewer_beams.py
+++ postprocessing/fewer_beams.py
@@ -4,13 +4,19 @@ import numpy as np
 import argparse
 import os
 from tqdm import tqdm
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../preprocessing"))
+
+from kitti_util import *
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--lidar_dir', type=str)
+    parser.add_argument('--calib_dir', type=str)
     parser.add_argument('--save_dir', type=str, default='./4beam_lidar')
     args = parser.parse_args()
 
     assert os.path.isdir(args.lidar_dir)
+    assert os.path.isdir(args.calib_dir)
     if not os.path.isdir(args.save_dir):
         os.makedirs(args.save_dir)
 
@@ -26,6 +32,15 @@ if __name__ == '__main__':
             ((theta >= -0.8) & (theta < -0.4)) |
             ((theta >= -1.6) & (theta < -1.2)) |
             ((theta >= -2.4) & (theta < -2.0)))
+        
+        # only front
+        mask &= lidar[:,0] > 0
+
+        # only inside image range
+        calib = Calibration('{}/{}.txt'.format(args.calib_dir, predix))
+        img = calib.project_velo_to_image(lidar[:,:3])
+        mask &= (img[:,0] > 0) & (img[:,0] < 1242) & (img[:,1] > 0) & (img[:,1] < 375)
+        
         fewer = lidar[mask]
         fewer.tofile('{}/{}.bin'.format(args.save_dir, predix))
 
